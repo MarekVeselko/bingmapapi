c36fc086161e854e87b0cb59eeaef495c52247b9
MarekVeselko <62647502+MarekVeselko@users.noreply.github.com>
2020-11-04 17:21:39 +0100

    "added search bar"

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-function loadMapScenario() {
-    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        center: new Microsoft.Maps.Location(51.516, -0.077),
-        zoom: 10
-    });
-    var zipCodes = ['w10', 'w2', 'w11', 'w6', 'se16', 'e9'];
-    var geoDataRequestOptions = {
-        entityType: 'Postcode2',
-        getAllPolygons: true
-    };
-    Microsoft.Maps.loadModule('Microsoft.Maps.SpatialDataService', function () {
-    
-        Microsoft.Maps.SpatialDataService.GeoDataAPIManager.getBoundary(zipCodes, geoDataRequestOptions, map, function (data) {
-            if (data.results && data.results.length > 0) {
-                map.entities.push(data.results[0].Polygons);
-            }
-        }, null, function errCallback(callbackState, networkStatus, statusMessage) {
-            console.log(callbackState);
-            console.log(networkStatus);
-            console.log(statusMessage);
-        });
-    });
-    
-}
+
+  var zipCodes = [""];
+
+
+  function loadMapScenario() {
+      var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
+      
+  
+  
+  
+       Microsoft.Maps.loadModule(['Microsoft.Maps.SpatialDataService', 'Microsoft.Maps.Search'], function () {
+          var searchManager = new Microsoft.Maps.Search.SearchManager(map);
+          var geocodeRequest = {
+              where: zipCodes[zipCodes.length-1],
+              callback: function (geocodeResult) {
+                  if (geocodeResult && geocodeResult.results && geocodeResult.results.length > 0) {
+                      map.setView({ bounds: geocodeResult.results[0].bestView });
+                      var geoDataRequestOptions = {
+                          entityType: 'Postcode2',
+                          getAllPolygons: true
+                      };
+                      //Use the GeoData API manager to get the boundary of New York City
+                      Microsoft.Maps.SpatialDataService.GeoDataAPIManager.getBoundary(geocodeResult.results[0].location, geoDataRequestOptions, map, function (data) {
+                          if (data.results && data.results.length > 0) {
+                              map.entities.push(data.results[0].Polygons);
+                          }
+                      }, null, function errCallback(networkStatus, statusMessage) {
+                          console.log(networkStatus);
+                          console.log(statusMessage);
+                      });
+                  }
+              },
+          };
+          searchManager.geocode(geocodeRequest);
+      });
+  
+  
+      
+      
+  }
+  
+  let postCode="";
+      
+  document.getElementById("post-code").addEventListener("change",function(){
+  postCode = String(document.getElementById("post-code").value).toLowerCase();  
+  })
+  
+  document.querySelector("button").addEventListener("click",function(){
+      zipCodes.push(postCode);
+      console.log = (zipCodes[zipCodes.length -1]);
+      loadMapScenario();
+    })
